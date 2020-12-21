899d16e227e5fac52b8b9473521efc3ded67db69
Abraham <nencho_1@hotmail.com>
2020-12-21 18:39:18 +0100

    I have to continue with errors

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-//Aqui vamos a crear rutas para las notas que recibiremos de la bbdd.
-router.get('/notes', (req, res) => {
-    res.send('Notes from database');
-});
-
-router.post('/notes/new-notes', (req, res) => {
-    console.log(req.body);
-    res.send('ok');
-});
-
 //Aqui creamos una ruta que es donde vamos a renderizar archivo.hbs new-notes.
 router.get('/notes/add', (req, res) => {
     res.render('notes/new-notes');
+});
+
+router.post('/notes/new-notes', (req, res) => {
+    const { title, textarea} = req.body;
+    const errors = [];  
+    if (!title) {   
+        errors.push({text: 'Inserte un titulo'})
+    } 
+    if (!textarea) {
+        errors.push({text: 'Inserte un texto'}) 
+    } 
+    if (errors.lenght > 0 ) {
+        res.render('/notes/new-notes', {
+            errors,
+            title,
+            textarea
+        });
+    } else {
+        res.send('ok');
+    };
+});
+
+//Aqui vamos a crear rutas para las notas que recibiremos de la bbdd.
+router.get('/notes', (req, res) => {
+    res.send('Notes from database');
 });
 
 
